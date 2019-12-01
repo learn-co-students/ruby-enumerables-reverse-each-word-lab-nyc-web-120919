367162688f461a45aa60f47c6010f548486ec50b
Benjamin Kagan <kagan.benjamin@gmail.com>
2019-12-01 17:33:07 +0000

    Done.

---
+++ reverse_each_word.rb
@@ -0,0 +1,9 @@
+# def reverse_each_word(string) 
+#  string.reverse.split.reverse.join(" ") 
+#   
+#  
+# end
+
+def reverse_each_word(string)
+  string.split.collect {|word| word.reverse}.join(" ")
+end
