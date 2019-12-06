b3293287c0a4cee3b13ca1e86b012811e8f66e4f
Alexandra Pestruyeva <apestruy@gmail.com>
2019-12-06 02:59:14 +0000

    Done.

---
+++ reverse_each_word.rb
@@ -0,0 +1,13 @@
+def reverse_each_word(sentence)
+  sentence_array = sentence.split(" ")
+  sentence_array.each do |word|
+    word.reverse!
+  end
+  reversed_sentence = sentence_array.join(" ")
+end
+
+def reverse_each_word(sentence)
+  sentence_array = sentence.split(" ")
+  sentence_array.collect { |word| word.reverse! }
+  reversed_sentence = sentence_array.join(" ")
+end 
